beb1b7dbdf4972580dca40db0e46ead87a983663
Di Zhou <ailox40@gmail.com>
2025-07-22 16:53:55 +0800

    Init

---
+++ VoronoiTerrain/VoronoiTerrainGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoronoiTerrainGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AVoronoiTerrainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AVoronoiTerrainGameMode();
+};
+
+
+
